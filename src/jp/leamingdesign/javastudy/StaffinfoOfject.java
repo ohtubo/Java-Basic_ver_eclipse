da906ea6ef22e6fdbf3f7e19a4e07e9eedb3ba3e
ohtubo <84015533+ohtubo@users.noreply.github.com>
2021-12-12 23:45:43 +0900

    [Add]_[jp.leamingdesign.javastudy]_[7]

--- src/jp/leamingdesign/javastudy/StaffinfoOfject.java
+++ src/jp/leamingdesign/javastudy/StaffinfoOfject.java
@@ -42,11 +42,25 @@ class info {
 	
 }
 
+class RemoteStaff extends info{
+	
+	public String location;
+	public RemoteStaff(String name, int staffid, String email) {
+		super(name, staffid, email);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+	
+}
+
 public class StaffinfoOfject {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-//		info yamada = new info("Taro Yamada",12345,"info@yahoo.co.jp");
+		
+		RemoteStaff tanaka = new RemoteStaff("Hanako tanaka",67890,"RemoteStaff@yahoo.co.jp");
+		tanaka.location = "大阪";
+		
+		info yamada = new info("Taro Yamada",12345,"info@yahoo.co.jp");
 //		yamada.name = "Taro Yamada";
 		
 		// System.out.println(yamada.name);
@@ -56,6 +70,12 @@ public class StaffinfoOfject {
 		System.out.println("氏名：" + yamada.getName());
 		System.out.println("社員番号：" + yamada.getStaffid());
 		System.out.println("Email：" + yamada.getEmail());
+		
+		System.out.println("[Remote社員情報]");
+		System.out.println("氏名：" + tanaka.getName());
+		System.out.println("社員番号：" + tanaka.getStaffid());
+		System.out.println("Email：" + tanaka.getEmail());
+		System.out.println("勤務地：" + tanaka.location);
 	}
 
 }
